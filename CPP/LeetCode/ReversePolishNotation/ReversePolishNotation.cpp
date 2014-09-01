87ffcb3a2133318bfcb988bde3adf0e0f66c75a4
Hao Lin <hao@aurnh.com>
2014-09-01 11:41:40 -0400

    polish Notion cpp ver

--- CPP/LeetCode/ReversePolishNotation/ReversePolishNotation.cpp
+++ CPP/LeetCode/ReversePolishNotation/ReversePolishNotation.cpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <string>
+#include <iostream>
 using namespace std;
 
 //I shoudl ask interviewer is the tokens promised to be correct or not
@@ -8,36 +10,43 @@ public:
         vector<int> stack;
         int a;
         int b;
-        for (int i=0; i<tokens.size(); i++){
+        for (unsigned i=0; i<tokens.size(); i++){
             if (stack.size() >=2){
-                int a = stack.pop_back();
-                int b = stack.pop_back();
+                a = stack.back();
+                stack.pop_back();
+                b = stack.back();
+                stack.pop_back();
             }
             if (tokens[i] == "+"){
                 stack.push_back(b+a);
-            }else if (tokens[i] == '-'){
+            }else if (tokens[i] == "-"){
                 stack.push_back(b-a);
-            }else if (tokens[i] == '*'){
+            }else if (tokens[i] == "*"){
                 stack.push_back(b*a);
-            }else if (tokens[i] == '/'){
+            }else if (tokens[i] == "/"){
                 stack.push_back(b/a);
             }else{
-                stack.push_back(tokens[i]);
+                stack.push_back(stoi(tokens[i]));
             }
-        return stack.pop_back()
-        }
+         }
+        return stack.back();
     }
 };
 
 int main(){
-    vecter<string> tokens1 = ["2", "1", "+", "3", "*"];
-    vecter<string> tokens1 = ["4", "13", "5", "/", "+"];
-    vecter<string> tokens3 = ["0","3","/"];
-    vecter<string> tokens4 =["-78","-33","196","+","-19","-","115","+","-","-99","/","-18","8","*","-86","-","-","16","/","26","-14","-","-","47","-","101","-","163","*","143","-","0","-","171","+","120","*","-60","+","156","/","173","/","-24","11","+","21","/","*","44","*","180","70","-40","-","*","86","132","-84","+","*","-","38","/","/","21","28","/","+","83","/","-31","156","-","+","28","/","95","-","120","+","8","*","90","-","-94","*","-73","/","-62","/","93","*","196","-","-59","+","187","-","143","/","-79","-89","+","-"];
+    string tokens_1[] = {"2", "1", "+", "3", "*"};
+    vector<string> tokens1(tokens_1, tokens_1 + sizeof(tokens_1)/sizeof(*tokens_1));
+//    vector<string> tokens2 = ["4", "13", "5", "/", "+"];
+//    vector<string> tokens3 = ["0","3","/"];
+    string tokens_4[] ={"-78","-33","196","+","-19","-","115","+","-","-99","/","-18","8","*","-86","-","-","16","/","26","-14","-","-","47","-","101","-","163","*","143","-","0","-","171","+","120","*","-60","+","156","/","173","/","-24","11","+","21","/","*","44","*","180","70","-40","-","*","86","132","-84","+","*","-","38","/","/","21","28","/","+","83","/","-31","156","-","+","28","/","95","-","120","+","8","*","90","-","-94","*","-73","/","-62","/","93","*","196","-","-59","+","187","-","143","/","-79","-89","+","-"};
+    vector<string> tokens4(tokens_4, tokens_4 + sizeof(tokens_4)/sizeof(*tokens_4));
 
-    cout << evalRPN(tokens1);
-    cout << evalRPN(tokens2);
-    cout << evalRPN(tokens3);
-    cout << evalRPN(tokens4);
+
+    Solution *solution =  new Solution;
+
+    cout << solution->evalRPN(tokens1);
+   // cout << solution->evalRPN(tokens2);
+   // cout << solution->evalRPN(tokens3);
+    cout << solution->evalRPN(tokens4);
 
 }
