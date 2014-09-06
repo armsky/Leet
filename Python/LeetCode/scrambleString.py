a7999d0bd76f502bee05e86e6926e3c99fafb3de
Hao Lin@cica <armsky1988@gmail.com>
2014-09-06 19:59:23 -0400

    scramble string Python accepted
    sort() use too much time

--- Python/LeetCode/scrambleString.py
+++ Python/LeetCode/scrambleString.py
@@ -37,37 +37,48 @@ We say that "rgtae" is a scrambled string of "great".
 Given two strings s1 and s2 of the same length, determine if s2 is a scrambled string of s1.
 
 Ideas: string S1 and S2, if they are scramble, patition them at any postion as s11, s12,
-and s21, s22. There must be s12 and s21 are scramble, AND s21 and s22 are scramble
+and s21, s22. There must be s12 and s21 are scramble AND s11 and s22 are scramble;
+OR s11,s21 are scramble AND s12 and s22 are scrable
 """
 
 class Solution:
     # @return a boolean
     def isScramble(self, s1, s2):
-        print s1, s2
+        #print s1, s2
+
         if len(s1) != len(s2):
             return False
+        size = len(s1)
         #extreme situation 1: s1 and s2 are only one character
-        if len(s1) == 1:
+        if size == 1:
             if s1[0] == s2[0]:
                 return True
             else:
                 return False
         #extreme situation 2: if s1 and s2 are empty
-        if len(s1) == 0:
+        if size == 0:
             return False
-        if ''.join(sorted(s1)) != ''.join(sorted(s2)):
-            #print ''.join(sorted(s1))
-            #print ''.join(sorted(s2))
-            return False
-        for i in range(len(s1)):
+        #use sort() might need too much time
+        #if ''.join(sorted(s1)) != ''.join(sorted(s2)):
+        #    return False
+        chars = [0 for x in range(26)]
+        for i in range(size):
+            chars[ord(s1[i])-ord('a')] +=1
+            chars[ord(s2[i])-ord('a')] -=1
+        for num in chars:
+            if num != 0:
+                return False
+        for i in range(size):
             if i >0:
                 s11, s12 = s1[:i], s1[i:]
                 s21, s22 = s2[:i], s2[i:]
-                print "s11, s12="+s11, s12
-                print "s21, s22="+s21, s22
+                #print "s11, s12="+s11 + " " + s12
+                #print "s21, s22="+s21 + " " + s22
 
                 if self.isScramble(s11, s21) and self.isScramble(s12, s22):
                     return True
+                #IMPORTANT!!!
+                s21, s22 = s2[:size-i], s2[size-i:]
                 if self.isScramble(s11, s22) and self.isScramble(s12, s21):
                     return True
         else:
@@ -76,3 +87,4 @@ class Solution:
 solution = Solution()
 print solution.isScramble("ccbbcaccbccbbbcca", "ccbbcbbaabcccbccc")
 print solution.isScramble("abb", "bba")
+print solution.isScramble("pcighfdjnbwfkohtklrecxnooxyipj", "npodkfchrfpxliocgtnykhxwjbojie")
